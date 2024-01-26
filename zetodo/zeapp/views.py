2baef82cff1132bfa91e5dbf456e1a0a4abc632b
= <=>
2024-01-26 15:16:32 +0000

    Config firts app

--- zetodo/zeapp/views.py
+++ zetodo/zeapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from typing import Dict
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+def index(request: HttpRequest) -> HttpResponse:
+    ctx : Dict = {
+        'title': "Todo 1",
+        'description' :"Todo liste 1"
+    }
+    return(render(request=request,context=ctx,template_name='index.html'))
