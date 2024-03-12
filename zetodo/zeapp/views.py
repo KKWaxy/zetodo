87e67d914d7a50743b912922ae84769f2b007031
= <=>
2024-03-12 07:51:34 +0100

    Fix long text input for title

--- zetodo/zeapp/views.py
+++ zetodo/zeapp/views.py
@@ -4,6 +4,7 @@ from uuid import UUID
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect
+from django.contrib import messages
 
 from .forms import TaskModelForm
 from .models import TaskModel
@@ -12,24 +13,19 @@ from .models import TaskModel
 def index(request: HttpRequest) -> HttpResponse:
 
     title = "ZeTodoApp"
-
     ctx : Dict = {
         'page_title': title
         }
-    
     if(request.method == 'POST'):
         form = TaskModelForm(request.POST)
         if(form.is_valid()):
             form.save()
-            tasks = TaskModel.objects.all()
-            ctx['tasks'] = tasks
-            return(render(request=request,context=ctx,template_name='zeapp/index.html'))
         else:
-            raise Exception(form.errors.as_json())
-    else:
-        tasks = TaskModel.objects.all()
-        ctx['tasks'] = tasks
-        return(render(request=request,context=ctx,template_name='zeapp/index.html'))
+            messages.error(request, form.errors)
+    tasks = TaskModel.objects.all()
+    ctx['tasks'] = tasks
+    
+    return(render(request=request,context=ctx,template_name='zeapp/index.html'))
 
 def delete_task(request: HttpRequest, task_id: UUID) -> HttpResponse:
 
