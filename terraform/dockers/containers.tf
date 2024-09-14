97a3ecf4ad519754075506a2edcd9a010cb0b37c
Wilfried KOUASSI <kkwaxy@gmail.com>
2024-09-14 03:37:57 +0200

    update .gitignore

--- terraform/dockers/containers.tf
+++ terraform/dockers/containers.tf
@@ -4,16 +4,16 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "nginx" {
-  count = var.nginx_host.index
+  count = var.nginx_host
   image = docker_image.nginx.image_id
   name = "nginx {{count.index }}"
+  depends_on = [ docker_image.nginx, docker_volume.shared_volume ]
   ports {
     internal = "80"
     external = "8081"
   }
   volumes {
     read_only = false
-    # host_path = abspath("nginx.cfg")
     container_path = "/usr/local/nginx/conf"
     volume_name = docker_volume.shared_volume.name
   }
